4e3277ba5362c367e1c2c8bb80c1eee7c6cbb579
Victória Lemos <vivi.ltl2505@gmail.com>
2020-08-06 02:26:31 -0300

    Update para o Heroku

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -37,18 +37,14 @@ function CadastroCategoria(){
     }
     
     useEffect(() => {
-        if(window.location.href.includes('localhost')) {
-            const URL = 'http://localhost:8080/categorias'; 
+            const URL = 'https://pianoflix.herokuapp.com/categorias'; 
             fetch(URL)
-            .then(async (respostaDoServer) =>{
-                if(respostaDoServer.ok) {
-                    const resposta = await respostaDoServer.json();
-                    setCategorias(resposta);
-                    return; 
-                }
-                throw new Error('Não foi possível pegar os dados');
+            .then(async (respostaDoServer) => {
+                const resposta = await respostaDoServer.json();
+                setCategorias([
+                    ...resposta,
+                ])
             })
-        }    
     }, [])
 
     return (
